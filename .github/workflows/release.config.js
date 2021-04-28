7c98ca3605febc8bf7dfde9d5cc45f155a4b128d
ksr30 <krishna.singh9926@gmail.com>
2021-04-28 20:50:37 +0530

    feat: some feature added

---
+++ .github/workflows/release.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/ksr30/rust-actions-cocktail",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+}
